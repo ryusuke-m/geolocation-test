09630a417306bc0e6f6041d02613ea84812b305c
ryusuke-m <miyaryu.tymuec@outlook.jp>
2024-01-06 04:52:15 +0900

    change!

--- script.js
+++ script.js
@@ -5,18 +5,15 @@ let options;
 function success(position) {
 //   const crd = position.coords;
     console.log("get current position"+position.timestamp)
-    document.getElementById("lat").innerHTML = 'Lat: ' + position.coords.latitude;
-    document.getElementById("lng").innerHTML = 'Lng: ' + position.coords.longitude;
-    document.getElementById("acc").innerHTML = 'Accuracy of position: ' + position.coords.accuracy;
-    document.getElementById("alt").innerHTML = 'Altitude: ' + position.coords.altitude;
-    document.getElementById("altAcc").innerHTML = 'Altitude Accuracy: ' + position.coords.altitudeAccuracy;
-    document.getElementById("heading").innerHTML = 'Heading: ' + position.coords.heading;
-    document.getElementById("speed").innerHTML = 'Speed: ' + position.coords.speed;
-    document.getElementById("timestamp").innerHTML = 'Timestamp: ' + position.timestamp;
-//   if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
-//     console.log("Congratulations, you reached the target");
-//     navigator.geolocation.clearWatch(id);
-//   }
+    document.getElementById("lat").innerHTML =  position.coords.latitude;
+    document.getElementById("long").innerHTML =  position.coords.longitude;
+    document.getElementById("acc").innerHTML =  position.coords.accuracy;
+    document.getElementById("alti").innerHTML = position.coords.altitude;
+    document.getElementById("altiAcc").innerHTML =  position.coords.altitudeAccuracy;
+    document.getElementById("heading").innerHTML =  position.coords.heading;
+    document.getElementById("speed").innerHTML = position.coords.speed;
+    document.getElementById("timestamp").innerHTML =  position.timestamp;
+    console.log(position)
 }
 
 function error(err) {
@@ -34,4 +31,12 @@ options = {
   maximumAge: 0,
 };
 
-id = navigator.geolocation.watchPosition(success, error, options);
+
+
+function onClickStartButton(){
+  // alert(id);
+  id = navigator.geolocation.watchPosition(success, error, options);
+}
+function onClickStopButton(){
+  navigator.geolocation.clearWatch(id);
+}
